e5f0fbe4f5f6a3d33c9d75186b47ef4e1b69a3f5
Angel Y <angel22.98@yandex.by>
2020-10-23 14:04:53 +0000

    13) Function import toActiveStatus

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -9,10 +9,22 @@ sap.ui.controller("dev.webapp.ext.controller.ObjectPageExt", {
 		sap.m.MessageToast.show("Hello World!!");
 	},
 	onClickActionCategoriesSections1: function (oEvent) {
+		var path = oEvent.getSource().getBindingContext().getPath() + "/StatusID";
 		this.getOwnerComponent().getModel().callFunction("/toActiveStatus",{
 			method: "GET",
 			success: function() {
-				this.getView();
+				var iStatusNew = 1,
+					iStatusActive = 2;
+					  
+				var iPreviousStatus = this.getOwnerComponent().getModel().getProperty(path);
+				
+				if(iPreviousStatus !== iStatusNew){
+					sap.m.MessageToast.show("Status is already in Active state");	
+				}else {
+					this.getOwnerComponent().getModel().setProperty(path, iStatusActive);
+					this.getOwnerComponent().getModel().submitChanges();
+					this.getOwnerComponent().getModel().refresh();
+				}
 			}.bind(this),
 			error: function() {
 				sap.m.MessageToast.show("Error status!!!");
